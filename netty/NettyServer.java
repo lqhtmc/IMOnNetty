d832ef823a96dd7a41b70ed8e4fc71377e94fdfd
lqhtmc <252705978@qq.com>
2020-03-23 22:15:36 +0800

    start simple netty server

---
+++ netty/NettyServer.java
@@ -0,0 +1,27 @@
+package com.sc.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+    public static void main(String[] args) {
+        //1、io线程模型
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioServerSocketChannel>() {
+
+                    protected void initChannel(NioServerSocketChannel nioServerSocketChannel) throws Exception {
+
+                    }
+                });
+
+        serverBootstrap.bind(8080);
+
+    }
+}
